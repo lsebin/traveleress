626fdf4e7b3e69f27e8ebddff64935ff038b99b8
ashleyteoh <teohashley@gmail.com>
2024-04-02 21:10:24 -0400

    added routes

--- cis412-app/src/App.js
+++ cis412-app/src/App.js
@@ -2,29 +2,48 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+import Home from "./pages/home";
+import Events from "./pages/events";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       
-      <header className="App-header">
+      {/* !!!! Move this to home.js !!!!! */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
+      </header>
 
         <div className="button">Let's explore </div> 
         
-        <div className="button">Meet up! </div> 
+        <div className="button">Meet up! </div>  */}
+
+
+        {/* !!! Uncomment your page when you create it !!! */}
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route exact path="/meetup" element={<Meetup />} /> */}
+            {/* <Route exact path="/meetup/coffee" element={<Coffee />} /> */}
+            
+            {/* <Route exact path="/meetup/coffee/manhattan-coffee-run" element={<Event1 />} />  */}
+            {/* ^^^ idk if this event should inherit from Events.js yet */}
+
+            <Route exact path="/events" element={<Events />} />
+            {/* <Route exact path="/profile" element={<Profile />} /> */}
+
+          </Routes>
+        </Router>
         
-      </header>
     </div>
   );
 }
---
+++ cis412-app/src/pages/events.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Events = () => {
+    return (
+        <div>
+            <h1>Your Events</h1>
+        </div>
+    );
+};
+ 
+export default Events;
